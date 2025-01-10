21c9562ab886fcd9e1a953312e3a9b7973183f78
Andrei V. Berezhkov <berezhkov.mail@yandex.ru>
2025-01-10 11:29:57 +0300

    add auth

--- config/account/urls.py
+++ config/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import register, index
+from .views import user_register, index, user_login, user_logout
 
 urlpatterns = [
-    path("register", register, name="account_register"),
     path("", index, name="account_index"),
-    ]
+    path("register", user_register, name="account_register"),
+    path("login", user_login, name="account_login"),
+    path("logout", user_logout, name="account_logout"),
+]
--- config/account/views.py
+++ config/account/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth import authenticate, login, logout
 
 from django.contrib.auth.models import User
 
 # Create your views here.
 
 
-def register(request):
+def user_register(request):
+    # регистрация == создание нового User
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-
             form.save()
             return redirect("account_index")
     form = UserRegistrationForm()
@@ -21,10 +22,25 @@ def user_login(request):
     if request.method == "POST":
         form = UserLoginForm(request.POST)
         if form.is_valid():
-            # сделать индентификацию и авторизацию
-            return redirect("account_index")
+            # получить логин и пароль
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            # аутентификацию пользователя
+            user = authenticate(
+                request, username=username, password=password
+            )  # User or None
+            # SELECT * FORM Users WHERE username = "username" AND password = "password"
+            if user is not None:
+                # авторизация пользователя
+                login(request, user=user)
+                return redirect("account_index")
     form = UserLoginForm()
     return render(request, "account/auth.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("account_index")
 
 
 def index(request):
